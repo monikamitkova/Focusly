267d9558e988473854feedb12a2fafcef0cee8b3
moikamitkova <mmonikkamitkova1@gmail.com>
2026-03-28 15:49:03 +0200

    Fix user stay logged in when refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import FocusPage from './domains/pages/FocusPage';
 
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AuthPage from './domains/pages/AuthPage';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+      return;
+    }
+
+    localStorage.removeItem("user");
+  }, [user]);
 
   return (
     <div className="min-h-screen">
